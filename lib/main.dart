b1ee9b1d64c044eeffc02825a36cc56103efa5b3
Andrey Rodrigues <andreyrodriues@gmail.com>
2026-04-28 10:33:13 -0300

    Add Firebase Config

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
-import 'screens/splash_screen.dart';
-import 'simulator/card_catalog.dart';
+import 'package:firebase_core/firebase_core.dart'; // 1. Importa o núcleo do Firebase
+import 'firebase_options.dart'; // 2. Importa o arquivo que o CLI gerou
+import 'screens/splash_screen.dart'; 
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // Importe no topo
 
-
-void main() async {
+// 3. O "main" agora é um Future assíncrono
+Future<void> main() async {
+  // 4. Garante que os blocos internos do Flutter estejam prontos
   WidgetsFlutterBinding.ensureInitialized();
-  await CardCatalog.load();
+
+  await dotenv.load(fileName: ".env"); // Carrega as variáveis do .env
   
+  // 5. Conecta o seu app ao Firebase usando as chaves geradas
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Inicia o app normalmente
   runApp(const MyApp());
 }
 
@@ -20,9 +30,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF183B1E)),
-        
       ),
-      home: const SplashScreen(), 
+      home: const SplashScreen(), // O app começa pela sua tela de carregamento
     );
   }
 }
